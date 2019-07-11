961a11b762f057d928eb1dc9d7de366131ffabcd
Joao Filipe Rocha <jfsllrocha@gmail.com>
2019-07-11 21:57:41 +0100

    Transito completo

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http = require('http');
 const iplocation = require("iplocation").default;
 const newsapiKey = require('./Credentials/newsapi.json').APIKey;
 const openWeatherapiKey = require('./Credentials/openWeather.json').APIKey;
+const hereKeys = require('./Credentials/here.json');
 const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI(newsapiKey);
 const state = require('./state.js');
@@ -16,6 +17,7 @@ async function start() {
     await fetchMajorCitiesByCountry(mainData);
     await fetchNewsForCountry(mainData);
     await fetchWeatherForMajorCities(mainData);
+    await fetchTrafficForMajorCities(mainData);
     await fetchTrendMusic(mainData);
 
     state.save(mainData);
@@ -131,19 +133,19 @@ async function start() {
                             const temp_maxC = tempData.main.temp - 273.15;
                             const temp_maxF = temp_maxC * (9 / 5) + 32;
                             const record = {
-                                    "city": majorCity.city,
-                                    "temp": tempData.main.temp,
-                                    "tempC": tempC.toFixed(2),
-                                    "tempF": tempF.toFixed(2),
-                                    "temp_min": tempData.main.temp_min,
-                                    "temp_minC": temp_minC.toFixed(2),
-                                    "temp_minF": temp_minF.toFixed(2),
-                                    "temp_maxC": temp_maxC.toFixed(2),
-                                    "temp_maxF": temp_maxF.toFixed(2),
-                                    "temp_max": tempData.main.temp_max,
-                                    "desc": tempData.weather.map(item => { return item.description })
-                                }
-                                //  console.log(record);
+                                "city": majorCity.city,
+                                "temp": tempData.main.temp,
+                                "tempC": tempC.toFixed(2),
+                                "tempF": tempF.toFixed(2),
+                                "temp_min": tempData.main.temp_min,
+                                "temp_minC": temp_minC.toFixed(2),
+                                "temp_minF": temp_minF.toFixed(2),
+                                "temp_maxC": temp_maxC.toFixed(2),
+                                "temp_maxF": temp_maxF.toFixed(2),
+                                "temp_max": tempData.main.temp_max,
+                                "desc": tempData.weather.map(item => { return item.description })
+                            }
+                            //  console.log(record);
                             resolve(record);
                         } catch (error) {
                             reject(error);
@@ -155,6 +157,53 @@ async function start() {
                 }
             })
         })
+
+    }
+
+    async function fetchTrafficForMajorCities(mainData) {
+        for (let index = 0; index < mainData.majorCities.length; index++) {
+            const city = mainData.majorCities[index];
+            const cityTraffic = await getCityTraffic(city);
+            mainData.majorCities[index].traffic = cityTraffic
+        }
+    }
+
+    function getCityTraffic(cityData) {
+        const url = `http://traffic.cit.api.here.com/traffic/6.3/incidents.json?prox=${cityData.latitude},${cityData.longitude},15000&criticality=0,1&maxresults=7&app_id=${hereKeys.AppID}&app_code=${hereKeys.AppCode}`
+        //console.log(url);
+        return new Promise((resolve, reject) => {
+            http.get(url, (res) => {
+                if (res.statusCode == 200) {
+                    res.setEncoding('utf8');
+                    let rawData = '';
+                    res.on('data', (chunk) => { rawData += chunk; });
+                    res.on('end', () => {
+                        try {
+                            const TRAFFIC_ITEMS = JSON.parse(rawData).TRAFFIC_ITEMS;
+                            if (!TRAFFIC_ITEMS)
+                                resolve();
+                            const trafficDesc = TRAFFIC_ITEMS.TRAFFIC_ITEM.map((item) => {
+                                const traffdesc = item.TRAFFIC_ITEM_DESCRIPTION.map((desc, idx) => {
+                                    if (desc && idx == 1)
+                                        return desc.value;
+                                });
+                                return traffdesc.filter((el) => { return el });
+                            })
+                            // console.log(trafficDesc);
+
+                            resolve(trafficDesc);
+                        } catch (error) {
+                            reject(error);
+                        }
+                    })
+                }
+            })
+        });
+    }
+
+
+
+    async function fetchTrendMusic(mainData) {
 
     }
 
